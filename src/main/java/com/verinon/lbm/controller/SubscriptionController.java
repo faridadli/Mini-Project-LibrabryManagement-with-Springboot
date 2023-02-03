0ee6d6d1ede39eec5e676a49c9bc8a0bba3dbf4b
tiaa <sitiroziah0212@gmail.com>
2023-02-03 11:35:25 +0800

    minor change

--- src/main/java/com/verinon/lbm/controller/SubscriptionController.java
+++ src/main/java/com/verinon/lbm/controller/SubscriptionController.java
@@ -17,82 +17,68 @@ public class SubscriptionController {
     @Autowired
     SubscriptionServices services;
 
-
     @RequestMapping("/show-listof-all-subscriptions")
-    public String showListOfSubscription(ModelMap model)
-    {
+    public String showListOfSubscription(ModelMap model) {
         model.put("listofsubscriptions", services.showAllSubscriptionsData());
         return "showallsubscriptions";
     }
 
     @GetMapping("/add-subscription")
-    public String addSubscriptionsShowPage(ModelMap model)
-    {
-        model.addAttribute("subscription", new SubscriptionPojo(0, "Tier", 0.0, 0, "Accessibility"));
+    public String addSubscriptionsShowPage(ModelMap model) {
+        model.addAttribute("subscription", new SubscriptionPojo(0, "Tier", 
+        0.0, 0, "Accessibility"));
         return "addSubscription";
     }
 
     @PostMapping("/add-subscription")
-    public String addSubscriptions(ModelMap model, SubscriptionPojo subscription)
-    {
-        services.addSubscription(subscription.getSubsc_tier(), subscription.getSubsc_fee(), subscription.getSubsc_limit(), subscription.getSubsc_accessibility());
-        //bookRepository.save(book);
+    public String addSubscriptions(SubscriptionPojo subscription) {
+        services.addSubscription(subscription.getSubsc_tier(), subscription.getSubsc_fee(),
+                subscription.getSubsc_limit(), subscription.getSubsc_accessibility());
         return "redirect:show-listof-all-subscriptions";
     }
 
     @RequestMapping("/delete-subscription")
-    public String deleteSubscriptioFromList(int id)
-    {
+    public String deleteSubscriptioFromList(int id) {
         services.delSubscription(id);
-        //bookRepository.deleteById(id);
-        return "redirect:show-listof-all-subscriptions";
-    }
-
-    @PostMapping("/edit-subscription")
-    public String editSubscriptionDetails(int id, @RequestParam String tier, @RequestParam double fee, @RequestParam int limit, @RequestParam String accessibility)
-    {
-        services.editSubscription(id ,tier, fee, limit, accessibility);
-        /*if(bookRepository.existsById(id))
-        {
-            services.editBook(id,bookname);
-            bookPojo.setBook_name(bookname);
-            bookRepository.save(bookPojo);
-        }*/
         return "redirect:show-listof-all-subscriptions";
     }
 
     @GetMapping("/edit-subscription")
-    public String editSubscriptionDetailsShowPage(ModelMap model, int id)
-    {
+    public String editSubscriptionDetailsShowPage(ModelMap model, int id) {
         model.put("editsubscription", services.showEditSubscription(id));
         return "editsubscription";
     }
 
+
+    @PostMapping("/edit-subscription")
+    public String editSubscriptionDetails(int id, @RequestParam String tier, @RequestParam double fee,
+            @RequestParam int limit, @RequestParam String accessibility) {
+        services.editSubscription(id, tier, fee, limit, accessibility);
+        return "redirect:show-listof-all-subscriptions";
+    }
+
+
     @GetMapping("/get-subscription-details")
-    public String getSubscriptionDetailsShowPage(ModelMap model)
-    {
+    public String getSubscriptionDetailsShowPage(ModelMap model) {
         model.put("searchsubscription", services.showAllSubscriptionsData());
-        //model.put("searchbook", bookRepository.findAll());
+        // model.put("searchbook", bookRepository.findAll());
         return "subscriptionsdetails";
     }
 
     @GetMapping("/get-subscription-tracking-details")
-    public String getSubscriptionTrackingDetailsShowPage(ModelMap model)
-    {
+    public String getSubscriptionTrackingDetailsShowPage(ModelMap model) {
         return "subscriptiontrackdetails";
     }
 
     @RequestMapping("/get-subscription-details-one")
-    public String getSubscriptionDetails(ModelMap model, @RequestParam String tier)
-    {
+    public String getSubscriptionDetails(ModelMap model, @RequestParam String tier) {
         model.put("clicksubscription", services.getSubscriptionDetails(tier));
-        //model.put("clickbook",bookRepository.findByBook_name(book_name));
+        // model.put("clickbook",bookRepository.findByBook_name(book_name));
         {
 
-            model.put("error","No Subjects are available");
+            model.put("error", "No Subjects are available");
         }
         return "getsubscriptiondetails";
     }
-
 
 }
